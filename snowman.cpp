547c86f3b2a2379dd9a3af9ba2b7b4df019af6fe
NathBen97 <74254759+Golem97@users.noreply.github.com>
2021-03-10 11:03:00 +0200

    last

--- snowman.cpp
+++ snowman.cpp
@@ -14,3 +14,79 @@ X(TTT)Y
 ARGS STRUCTUR : HNLRXYTB
 ================================================================
 */
+#include <iostream>
+#include "snowman.hpp"
+#include <array>
+#include <string>
+#include <exception>
+using namespace std;
+
+namespace ariel
+{
+
+    string snowman(int input)
+    {
+
+        //Check if input is good 
+        if (input < MINIMUM || input > MAXIMUM)
+        {
+            __throw_invalid_argument("Error! Not a valid input");
+        }
+        else
+        {
+
+            int *arr = new int[SIZE];
+            //Checks for each digit if its correct
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (input % BASE > 4 || input % BASE < 1)
+                {
+                    __throw_invalid_argument("Error! Not a valid input");
+                }
+                arr[LAST_INDEX - i] = input % BASE;
+                input /= BASE;
+            }
+
+            //Creat array For each parts of : HNLRXYTB
+
+            //Empty, Straw hat, Mexican Hat,  Fez, Russian Hat
+            const array<string, 5> hats = {"", "_===_\n", " ___ \n .....\n", "  _  \n  /_\\ \n", " ___ \n (_*_)\n"};
+
+            //Empty, Normal, Dot, Line, None
+            const array<string, 5> noses = {"", ",", ".", "_", " "};
+
+            //Empty, Dot ,Bigger Dot, Biggest Dot, Closed
+            const array<string, 5> right_Eye = {"", ".", "o", "O", "-"};
+
+            //Empty, Dot ,Bigger Dot, Biggest Dot, Closed
+            const array<string, 5> left_Eye = {"", ".", "o", "O", "-"};
+            
+            //Empty,  Normal Arm,  Upwards Arm, Downwards Arm, None
+            const array<string, 5> high_Left = {"", " ", "\\", " ", " "};
+
+            //Empty,  Normal Arm,  Upwards Arm, Downwards Arm, None
+            const array<string, 5> low_Left = {"", "<", " ", "/", " "};
+           
+            //Empty,  Normal Arm,  Upwards Arm, Downwards Arm, None
+            const array<string, 5> low_Right = {"", ">", " ", "\\", " "};
+
+            //Empty,Buttons, vest, Inwards Arms, None
+            const array<string, 5> torso = {"", " : ", "] [", "> <", "   "};
+
+            //Empty,  Normal Arm,  Upwards Arm, Downwards Arm, None 
+            const array<string, 5> high_Right = {"", " ", "/", " ", " "};
+
+            //Empty, Buttons, feet, Flat, None
+            const array<string, 5> base = {"", " : ", "\" \"", "___", "   "};
+
+            //Constuction of the return ans with peek each value in corresponding index of is array.
+
+            string snw = " " + hats[arr[0]] +
+                         highs_Left[arr[4]] + "(" + left_Eye[arr[2]] + noses[arr[1]] + right_Eye[arr[3]] + ")" + high_Right[arr[5]] + "\n" +
+                         low_Left[arr[4]] + "(" + torso[arr[6]] + ")" + low_Right[arr[5]] + "\n (" +
+                         base[arr[7]] + ")";
+            return snw;
+        }
+    }
+}
+
--- snowman.hpp
+++ snowman.hpp
@@ -1,9 +1,9 @@
 #include <string>
 #pragma once
 //Defining Constant Expressions for the Minimum and Maximum values of input
-const int MIN_VAL = 11111111;
-const int MAX_VAL = 44444444;
-const int LEN = 8, FINAL_INDEX = 7;
+const int MINIMUM = 11111111;
+const int MAXIMUM = 44444444;
+const int SIZE = 8, LAST_INDEX = 7;
 const int BASE = 10;
 namespace ariel
 {
